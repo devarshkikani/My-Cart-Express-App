91a329ff51b62576522012bab386c1d68aee19f3
devarsh kikani <devarshkikani2904@gmail.com>
2022-11-23 19:56:09 +0530

    design my reward screen

--- lib/screens/more_screen/my_rewards_screen.dart
+++ lib/screens/more_screen/my_rewards_screen.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:my_cart_express/constant/sizedbox.dart';
 import 'package:my_cart_express/theme/colors.dart';
 import 'package:my_cart_express/theme/text_style.dart';
 import 'package:my_cart_express/widget/app_bar_widget.dart';
@@ -42,6 +44,7 @@ class _MyRewardsScreenState extends State<MyRewardsScreen> {
 
   Widget bodyView() {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Row(
           children: [
@@ -50,6 +53,77 @@ class _MyRewardsScreenState extends State<MyRewardsScreen> {
               style: regularText18,
             ),
           ],
+        ),
+        height20,
+        Text(
+          'Referral Link',
+          style: regularText16,
+        ),
+        height10,
+        Container(
+          width: Get.width,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 15,
+            vertical: 20,
+          ),
+          decoration: BoxDecoration(
+            color: greyColor.withOpacity(0.5),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Click copy to the referral to your clipboard',
+              ),
+              height10,
+              Container(
+                width: Get.width,
+                padding: const EdgeInsets.all(
+                  10,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: whiteColor,
+                  border: Border.all(
+                    color: Colors.grey,
+                  ),
+                ),
+                child: Text(
+                  'https://web.whatsapp.com/',
+                  style: lightText13.copyWith(
+                    color: primary,
+                  ),
+                ),
+              ),
+              height10,
+              GestureDetector(
+                onTap: () async {
+                  await Clipboard.setData(
+                    const ClipboardData(text: "your text"),
+                  );
+                  Get.snackbar(
+                    'Link Copied',
+                    '',
+                    snackPosition: SnackPosition.BOTTOM,
+                    padding: const EdgeInsets.all(15),
+                    margin: const EdgeInsets.all(15),
+                    messageText: const SizedBox(),
+                  );
+                },
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    'Copy',
+                    style: regularText14.copyWith(
+                      color: primary,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ],
     );
