0a510dc2d850f70d8234755ffdf82b383f2f0212
devarsh kikani <devarshkikani2904@gmail.com>
2022-11-24 17:21:53 +0530

    design auth pickup screen

--- lib/screens/more_screen/auth_pickup_screen.dart
+++ lib/screens/more_screen/auth_pickup_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:my_cart_express/constant/sizedbox.dart';
 import 'package:my_cart_express/theme/colors.dart';
 import 'package:my_cart_express/theme/text_style.dart';
 import 'package:my_cart_express/widget/app_bar_widget.dart';
+import 'package:my_cart_express/widget/input_text_field.dart';
+import 'package:my_cart_express/widget/validator.dart';
 
 class AuthPickupScreen extends StatefulWidget {
   const AuthPickupScreen({super.key});
@@ -12,6 +15,12 @@ class AuthPickupScreen extends StatefulWidget {
 }
 
 class _AuthPickupScreenState extends State<AuthPickupScreen> {
+  TextEditingController firstName = TextEditingController();
+  TextEditingController lastName = TextEditingController();
+  TextEditingController mobileNumber = TextEditingController();
+  TextEditingController idNumber = TextEditingController();
+  String? idType;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,16 +51,138 @@ class _AuthPickupScreenState extends State<AuthPickupScreen> {
 
   Widget bodyView() {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          children: [
-            Text(
-              'Authorise Pick Up',
-              style: regularText18,
-            ),
-          ],
+        Text(
+          'Authorise Pick Up',
+          style: regularText18,
         ),
+        formView(),
       ],
+    );
+  }
+
+  Widget formView() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          height30,
+          Text(
+            'First Name',
+            style: regularText14,
+          ),
+          height10,
+          TextFormFieldWidget(
+            hintText: 'First name',
+            controller: firstName,
+            validator: (value) => Validators.validateText(value, 'First Name'),
+          ),
+          height15,
+          Text(
+            'Last Name',
+            style: regularText14,
+          ),
+          height10,
+          TextFormFieldWidget(
+            hintText: 'Last name',
+            controller: lastName,
+            validator: (value) => Validators.validateText(value, 'Last Name'),
+          ),
+          height15,
+          Text(
+            'Mobile Number',
+            style: regularText14,
+          ),
+          height10,
+          TextFormFieldWidget(
+            hintText: 'Mobile Number',
+            controller: mobileNumber,
+            validator: (value) =>
+                Validators.validateText(value, 'Mobile Number'),
+          ),
+          height15,
+          Text(
+            'ID Type',
+            style: regularText14,
+          ),
+          height10,
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                color: blackColor,
+              ),
+            ),
+            child: DropdownButton<String>(
+              underline: const SizedBox(),
+              isExpanded: true,
+              value: idType,
+              hint: Text(
+                'Select ID Type',
+                style: lightText16.copyWith(
+                  color: Theme.of(context).hintColor,
+                ),
+              ),
+              icon: const Icon(
+                Icons.keyboard_arrow_down_rounded,
+                color: secondary,
+              ),
+              items: <String>[
+                "Driver's License",
+                'National Id',
+                'Other',
+                'Passport'
+              ].map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(
+                    value,
+                  ),
+                );
+              }).toList(),
+              onChanged: (_) {
+                idType = _;
+                setState(() {});
+              },
+            ),
+          ),
+          height15,
+          Text(
+            'ID Number',
+            style: regularText14,
+          ),
+          height10,
+          TextFormFieldWidget(
+            hintText: 'ID Number',
+            controller: idNumber,
+            validator: (value) => Validators.validateText(value, 'ID Number'),
+          ),
+          height20,
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: secondary,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(5)),
+              ),
+            ),
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {}
+            },
+            child: const Text(
+              'SUBMIT',
+              style: TextStyle(
+                letterSpacing: 0.5,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
