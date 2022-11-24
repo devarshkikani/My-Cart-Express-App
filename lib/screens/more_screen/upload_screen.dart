697882e552f6de3edb4edf108ecdf9a98e86e08a
devarsh kikani <devarshkikani2904@gmail.com>
2022-11-24 17:55:42 +0530

    design upload screen

--- lib/screens/more_screen/upload_screen.dart
+++ lib/screens/more_screen/upload_screen.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:my_cart_express/constant/sizedbox.dart';
 import 'package:my_cart_express/theme/colors.dart';
 import 'package:my_cart_express/theme/text_style.dart';
 import 'package:my_cart_express/widget/app_bar_widget.dart';
+import 'package:my_cart_express/widget/input_text_field.dart';
+import 'package:my_cart_express/widget/validator.dart';
 
 class UploadFileScreen extends StatefulWidget {
   const UploadFileScreen({super.key});
@@ -12,6 +16,9 @@ class UploadFileScreen extends StatefulWidget {
 }
 
 class _UploadFileScreenState extends State<UploadFileScreen> {
+  TextEditingController type = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,17 +48,287 @@ class _UploadFileScreenState extends State<UploadFileScreen> {
   }
 
   Widget bodyView() {
-    return Column(
-      children: [
-        Row(
-          children: [
-            Text(
-              'Upload File',
-              style: regularText18,
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                'Upload File',
+                style: regularText18,
+              ),
+            ],
+          ),
+          height20,
+          Text(
+            'SELECT YOUR FILE TYPE',
+            style: regularText14,
+          ),
+          height10,
+          TextFormFieldWidget(
+            hintText: 'Select Product',
+            controller: type,
+            readOnly: true,
+            onTap: () {
+              showBottomSheet(context, 1);
+            },
+            suffixIcon: const Icon(
+              Icons.keyboard_arrow_down_rounded,
+              color: primary,
             ),
-          ],
-        ),
-      ],
+            validator: (value) => Validators.validateText(value, 'File Type'),
+          ),
+          height15,
+          Row(
+            children: [
+              Text(
+                'UPLOAD ID',
+                style: lightText14,
+              ),
+              Text(
+                ' (PDF/JPEG/PNG/TIFF)',
+                style: lightText14.copyWith(
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+          height15,
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 20,
+            ),
+            decoration: BoxDecoration(
+              color: greyColor.withOpacity(.5),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Invoice file',
+                      ),
+                      Text(
+                        '(filename.txt)',
+                        style: lightText14.copyWith(
+                          color: primary,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: greyColor,
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                          ),
+                        ),
+                        onPressed: () {
+                          selectFileType(context);
+                        },
+                        child: const Text(
+                          'Click to select file...',
+                          style: TextStyle(
+                            letterSpacing: 0.5,
+                            color: primary,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          height15,
+          const Text(
+            'Upload File to Your Account \nDO NOT UPLOAD Shipping Invoice Here',
+          ),
+          height20,
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: secondary,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(5)),
+              ),
+            ),
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {}
+            },
+            child: const Text(
+              'SUBMIT',
+              style: TextStyle(
+                letterSpacing: 0.5,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void selectFileType(
+    BuildContext context,
+  ) {
+    showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      backgroundColor: offWhite,
+      builder: (BuildContext context) {
+        return SizedBox(
+          height: 250,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                height20,
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: whiteColor,
+                    textStyle: const TextStyle(color: blackColor),
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(
+                        color: blackColor,
+                      ),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: const Text(
+                    "CHOOSE IMAGE FROM GALLERY",
+                    style: TextStyle(
+                      color: blackColor,
+                    ),
+                  ),
+                ),
+                height10,
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: whiteColor,
+                    textStyle: const TextStyle(color: blackColor),
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(
+                        color: blackColor,
+                      ),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: const Text(
+                    "CHOOSE DOCUMENT",
+                    style: TextStyle(
+                      color: blackColor,
+                    ),
+                  ),
+                ),
+                height10,
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: blackColor,
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    Get.back();
+                  },
+                  child: const Text(
+                    'CANCEL',
+                    style: TextStyle(
+                      letterSpacing: 0.5,
+                    ),
+                  ),
+                ),
+                height20,
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void showBottomSheet(BuildContext context, int index) {
+    showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      builder: (BuildContext context) {
+        return SizedBox(
+          height: 250,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: CupertinoPicker(
+                  itemExtent: 40,
+                  magnification: 1.33,
+                  squeeze: 1.2,
+                  useMagnifier: true,
+                  looping: true,
+                  onSelectedItemChanged: (int i) {
+                    type.text = 'Hello';
+                  },
+                  children: List.generate(
+                    10,
+                    (index) => Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: Text(
+                        'Hello',
+                        style: mediumText18.copyWith(
+                          color: primary,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    maximumSize: Size(Get.width, 50),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text(
+                    'SELECT',
+                    style: TextStyle(
+                      letterSpacing: 1,
+                    ),
+                  ),
+                ),
+              ),
+              height10,
+            ],
+          ),
+        );
+      },
     );
   }
 }
