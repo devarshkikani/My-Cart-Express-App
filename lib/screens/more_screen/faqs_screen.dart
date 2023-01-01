36475451a024cc66faa8ca1df7ae57f982737a3b
Devarsh kikani <devarshkikani2904@gmail.com>
2023-01-01 12:17:54 +0530

    integrated faqs API

--- lib/screens/more_screen/faqs_screen.dart
+++ lib/screens/more_screen/faqs_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:get/get.dart';
+import 'package:my_cart_express/constant/app_endpoints.dart';
 import 'package:my_cart_express/constant/sizedbox.dart';
 import 'package:my_cart_express/theme/colors.dart';
 import 'package:my_cart_express/theme/text_style.dart';
+import 'package:my_cart_express/utils/network_dio.dart';
 import 'package:my_cart_express/widget/app_bar_widget.dart';
 import 'package:my_cart_express/widget/input_text_field.dart';
 
@@ -15,6 +18,42 @@ class FAQSScreen extends StatefulWidget {
 
 class _FAQSScreenState extends State<FAQSScreen> {
   TextEditingController searchContorller = TextEditingController();
+
+  RxList faqList = [].obs;
+  RxList serachedList = [].obs;
+  RxString searchData = ''.obs;
+
+  @override
+  void initState() {
+    super.initState();
+    getFaqList();
+    debounce<String>(searchData, validations,
+        time: const Duration(milliseconds: 500));
+  }
+
+  validations(String string) {
+    serachedList.value = faqList
+        .where((e) =>
+            e['question'].toLowerCase().contains(
+                  string.toLowerCase(),
+                ) ||
+            e['answer'].toUpperCase().contains(
+                  string.toUpperCase(),
+                ))
+        .toList();
+  }
+
+  void getFaqList() async {
+    Map<String, dynamic>? response = await NetworkDio.getDioHttpMethod(
+      url: ApiEndPoints.apiEndPoint + ApiEndPoints.faqList,
+      context: context,
+    );
+    if (response != null) {
+      faqList.value = response['data'];
+      serachedList.value = response['data'];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,6 +99,9 @@ class _FAQSScreenState extends State<FAQSScreen> {
               child: TextFormFieldWidget(
                 hintText: 'Search',
                 controller: searchContorller,
+                onChanged: (String? value) {
+                  searchData.value = value ?? '';
+                },
               ),
             ),
           ],
@@ -71,62 +113,71 @@ class _FAQSScreenState extends State<FAQSScreen> {
         ),
         height10,
         Expanded(
-          child: ListView.builder(
-            itemCount: 10,
-            padding: EdgeInsets.zero,
-            itemBuilder: (BuildContext context, int index) => Container(
-              margin: const EdgeInsets.fromLTRB(0, 10, 18, 10),
-              decoration: BoxDecoration(
-                color: primary.withOpacity(0.2),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Theme(
-                data: Theme.of(context)
-                    .copyWith(dividerColor: Colors.transparent),
-                child: ExpansionTile(
-                  title: Text(
-                    'How long does my package take to get to Jamaica?',
-                    style: regularText14,
-                  ),
-                  tilePadding: const EdgeInsets.only(
-                      left: 15, right: 15, top: 0, bottom: 0),
-                  childrenPadding: EdgeInsets.zero,
-                  iconColor: blackColor,
-                  collapsedIconColor: blackColor,
-                  // collapsedBackgroundColor: primary.withOpacity(0.2),
-                  // backgroundColor: primary.withOpacity(0.5),
-                  children: <Widget>[
-                    Container(
-                      padding: const EdgeInsets.all(10),
-                      decoration: const BoxDecoration(
-                        color: greyColor,
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(10),
-                          bottomRight: Radius.circular(10),
-                        ),
-                      ),
-                      width: Get.width,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Answer',
-                            style: regularText16,
-                          ),
-                          height10,
-                          const Text(
-                            'Once received in Miami and processed, it can take 3-6 business days without airline delays.',
-                          ),
-                        ],
-                      ),
+          child: Obx(
+            () => ListView.builder(
+              itemCount: serachedList.length,
+              padding: EdgeInsets.zero,
+              itemBuilder: (BuildContext context, int index) => Container(
+                margin: const EdgeInsets.fromLTRB(0, 10, 18, 10),
+                decoration: BoxDecoration(
+                  color: primary.withOpacity(0.2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Theme(
+                  data: Theme.of(context)
+                      .copyWith(dividerColor: Colors.transparent),
+                  child: ExpansionTile(
+                    title: Text(
+                      serachedList[index]['question'].trim(),
+                      style: regularText14,
                     ),
-                  ],
+                    tilePadding:
+                        const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                    childrenPadding: EdgeInsets.zero,
+                    iconColor: blackColor,
+                    collapsedIconColor: blackColor,
+                    // collapsedBackgroundColor: primary.withOpacity(0.2),
+                    // backgroundColor: primary.withOpacity(0.5),
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: const BoxDecoration(
+                          color: greyColor,
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10),
+                          ),
+                        ),
+                        width: Get.width,
+                        child: html(serachedList[index]['answer'].trim()),
+                        // child: Column(
+                        //   crossAxisAlignment: CrossAxisAlignment.start,
+                        //   children: [
+                        //     Text(
+                        //       'Answer',
+                        //       style: regularText16,
+                        //     ),
+                        //     height10,
+                        //     const Text(
+                        //       'Once received in Miami and processed, it can take 3-6 business days without airline delays.',
+                        //     ),
+                        //   ],
+                        // ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
         ),
       ],
+    );
+  }
+
+  Widget html(String data) {
+    return Html(
+      data: data,
     );
   }
 }
