4813b462ae57d810cdc50b7b99f4e85068f03d5f
devarsh kikani <devarshkikani2904@gmail.com>
2023-02-06 13:34:51 +0530

    implement images on welcome screen

--- lib/screens/authentication/welcome_screen.dart
+++ lib/screens/authentication/welcome_screen.dart
@@ -1,16 +1,40 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:get/get.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:my_cart_express/constant/app_endpoints.dart';
 import 'package:my_cart_express/screens/authentication/sign_up/sign_up_bindings.dart';
 import 'package:my_cart_express/screens/authentication/sign_up/sign_up_screen.dart';
 import 'package:my_cart_express/theme/colors.dart';
 import 'package:my_cart_express/theme/text_style.dart';
 import 'package:my_cart_express/constant/sizedbox.dart';
-import 'package:my_cart_express/constant/default_images.dart';
 import 'package:my_cart_express/screens/authentication/sign_in/sign_in_screen.dart';
+import 'package:my_cart_express/utils/network_dio.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  RxList imagesList = [].obs;
+  @override
+  void initState() {
+    super.initState();
+    getLoginImages(context);
+  }
+
+  Future<void> getLoginImages(BuildContext context) async {
+    Map<String, dynamic>? response = await NetworkDio.getDioHttpMethod(
+      url: ApiEndPoints.apiEndPoint + ApiEndPoints.loginImages,
+      context: context,
+    );
+    if (response != null) {
+      imagesList.value = response['data'];
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,19 +50,53 @@ class WelcomeScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Image.asset(
-                      boxImage,
+                  Container(
+                    margin: const EdgeInsets.all(10),
+                    child: Obx(
+                      () => imagesList.isEmpty
+                          ? const SizedBox(
+                              height: 450,
+                            )
+                          : CarouselSlider.builder(
+                              itemCount: imagesList.length,
+                              options: CarouselOptions(
+                                enlargeCenterPage: true,
+                                padEnds: true,
+                                viewportFraction: 1.0,
+                                scrollPhysics:
+                                    const NeverScrollableScrollPhysics(),
+                                height: 450,
+                                autoPlay: true,
+                                autoPlayInterval: const Duration(seconds: 5),
+                              ),
+                              itemBuilder: (context, i, id) {
+                                return Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15),
+                                    border: Border.all(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(15),
+                                    child: Image.network(
+                                      imagesList[i]['image_url'],
+                                      width: Get.width,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
                     ),
                   ),
-                  Text(
-                    'Shopping online has never been easier with your preffered shipping partner',
-                    style: regularText14.copyWith(
-                      fontFamily: 'Montserrat',
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
+                  // Text(
+                  //   'Shopping online has never been easier with your preffered shipping partner',
+                  //   style: regularText14.copyWith(
+                  //     fontFamily: 'Montserrat',
+                  //   ),
+                  //   textAlign: TextAlign.center,
+                  // ),
                 ],
               ),
             ),
