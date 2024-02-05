813297e5a94c97b651d89aea33e088e9d26a7d54
devarsh kikani <devarshkikani2904@gmail.com>
2024-02-05 11:28:47 +0530

    descres app version

--- lib/order_tracking_app/utils/global_singleton.dart
+++ lib/order_tracking_app/utils/global_singleton.dart
@@ -5,6 +5,6 @@ class GlobalSingleton {
   GlobalSingleton._internal();
   static final GlobalSingleton globalSingleton = GlobalSingleton._internal();
 
-  static String appVersion = '3.0.24';
+  static String appVersion = '3.0.23';
   static Map userDetails = {};
 }
