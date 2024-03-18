c193da8c300bba01fd93da6eb32ec6e5cee24996
Devarsh Kikani <devarshkikani2904@gmail.com>
2024-03-18 10:12:33 +0530

    IOS screen shot error fixed

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -22,13 +22,14 @@ import Firebase
 
 
 extension UIWindow {
-func makeSecure() {
-    let field = UITextField()
-    field.isSecureTextEntry = true
-    self.addSubview(field)
-    field.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-    field.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-    self.layer.superlayer?.addSublayer(field.layer)
-    field.layer.sublayers?.first?.addSublayer(self.layer)
-  }
+    func makeSecure() {
+        let field = UITextField()
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: field.frame.self.width, height: field.frame.self.height))
+        field.isSecureTextEntry = true
+        self.addSubview(field)
+        self.layer.superlayer?.addSublayer(field.layer)
+        field.layer.sublayers?.last!.addSublayer(self.layer)
+        field.leftView = view
+        field.leftViewMode = .always
+    }
 }
