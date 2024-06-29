7f6ba90a0f0593a67f6543ead9fe555b90ae0d5e
devarsh-ssh <devarshkikani2904@gmail.com>
2024-06-29 20:17:08 +0530

    change branch list api

--- lib/order_tracking_app/constant/app_endpoints.dart
+++ lib/order_tracking_app/constant/app_endpoints.dart
@@ -10,10 +10,10 @@ class ApiEndPoints {
   static String signUp = 'authentication/registration';
   static String changePassword = 'user/change_password';
   static String editName = 'user/edit_name';
-  static String editPhone = 'user/edit_phone  ';
+  static String editPhone = 'user/edit_phone';
   static String forgotPassword = 'authentication/forgot_password';
   static String resetPassword = 'authentication/reset_password';
-  static String branches = 'branch_listing';
+  static String branches = 'sign_up_branch_listing';
   static String switchBranch = 'switch_branch';
   static String location = 'locations';
 
